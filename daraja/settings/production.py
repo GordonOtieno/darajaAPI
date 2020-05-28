b0f6d0f15721035da46db07f6875c4dfc79d650d
GordonOtieno <otienogordon95@gmail.com>
2020-05-28 04:31:17 -0900

    formated settings

--- daraja/settings/production.py
+++ daraja/settings/production.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'rest_framework',
-     'storages',
+    'rest_framework',
+    'storages',
 ]
 
 MIDDLEWARE = [
