483a171f3a28d4f14e15c6ac7400e63f2580aea0
GordonOtieno <otienogordon95@gmail.com>
2020-05-29 04:21:32 -0900

    update on req

--- daraja/settings/production.py
+++ daraja/settings/production.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'storages',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
