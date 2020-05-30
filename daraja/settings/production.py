e603f6fefd7784b06ab25a1db4f989e31a765448
GordonOtieno <otienogordon95@gmail.com>
2020-05-30 14:08:44 -0900

    corrected allowed hosts

--- daraja/settings/production.py
+++ daraja/settings/production.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://floating-depths-21744.herokuapp.com",]
+ALLOWED_HOSTS = ["floating-depths-21744.herokuapp.com",]
 
 
 # Application definition
