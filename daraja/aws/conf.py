9a5f20972df21eeb2fc0f165e61a94eb9a4ddac9
GordonOtieno <otienogordon95@gmail.com>
2020-05-30 11:47:07 -0900

    troubleshooting

--- daraja/aws/conf.py
+++ daraja/aws/conf.py
@@ -1,20 +1,22 @@
 
 import datetime
 
-AWS_ACCESS_KEY_ID = "AKIARNFKIVVGBQ45LQFA"
-AWS_SECRET_ACCESS_KEY = "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"
+from storages.backends.s3boto3 import S3Boto3Storage
+
+AWS_ACCESS_KEY_ID = "AKIARNFKIVVGKPSHMSGR"
+AWS_SECRET_ACCESS_KEY = "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"
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = False
 
-DEFAULT_FILE_STORAGE = 'daraja.aws.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'daraja.aws.utils.StaticRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'daraja-api'
 S3DIRECT_REGION = 'eu-west-2'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
-#STATIC_URL = S3_URL + 'static/'
+STATIC_URL = S3_URL + 'static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 two_months = datetime.timedelta(days=61)
@@ -25,18 +27,3 @@ AWS_HEADERS = {
     'Expires': expires,
     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
 }
-
-
-
-AWS_STORAGE_BUCKET_NAME = 'daraja-api'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
